45905fd9735d6aaa730bf0bda89f031b5962e5c8
sirblazzy <emmanuelfranklin2020@yahoo.com>
2022-09-08 15:39:19 -0700

    4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
 
-include <stdio.h>
-
+/*
+ *  * main - Entry point
+ *   *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *       *
+ *      * Return: Always 0 (Success)
+ *       */
 int main(void)
-{ 
-	puts("Programming is like building a multilingual puzzle\n");
-       	return (0);
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-
-
+Footer
