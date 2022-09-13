3cb6c406ca25f509dd3ac085c7628eecd02cda39
sirblazzy <emmanuelfranklin2020@yahoo.com>
2022-09-13 09:57:58 -0700

    putchar c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
 
  *  main - Entry point 
  *  
+ *  Description: print _putchar using putchar prototype
+ *
  *  Return: Always 0 (Success)
  */
 int main(void){
 	
-	char *sh = "Holberton";
-	
+	char *sh = "putchar";
 	while (*sh)	
 
 	{
 	
 		_putchar(*sh);
-
 		sh++;	
 	}
 
 	_putchar('\n');
-
 	return (0);
 }
