ca78a1f3c7a83f9bb909d08ff24a1314b92db4fd
sirblazzy <emmanuelfranklin2020@yahoo.com>
2022-09-27 15:38:29 -0700

    it my world

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,13 @@
 
 /**
  *   * _memset - Fills memory with a constant byte b
- *     * @s: Char address to be filled
- *       * @b: Byte to fill
- *         * @n: number of interger times to fill
- *           * Return: Pointer to the memory area s
- *             */
+ *    * @s: Char address to be filled
+ *    * @b: Byte to fill
+ *    * @n: number of interger times to fill
+ *    * Return: Pointer to the memory area s
+ * */
 char *_memset(char *s, char b, unsigned int n)
 {
-		memset(s, b, n);
-			return (s);
+	memset(s, b, n);
+	return (s);
 }
