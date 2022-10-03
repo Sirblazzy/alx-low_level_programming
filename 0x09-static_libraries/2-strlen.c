4edc9a9505012bdf8774c14eb87e89f65cf295ac
sirblazzy <emmanuelfranklin2020@yahoo.com>
2022-10-03 10:49:58 -0700

    its my world

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *   * _strlen - Returns the length of a string
+ *     * @s: Input character
+ *       * Return: zero (Nothing)
+ *         */
+int _strlen(char *s)
+{
+		int count = 0;
+
+			while (*(s + count) != '\0')
+					{
+								count++;
+									}
+				return (count);
+}
