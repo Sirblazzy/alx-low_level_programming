bf4b277ffcf5514ebe01b68d7bdbef20e981eccf
sirblazzy <emmanuelfranklin2020@yahoo.com>
2022-10-03 23:51:49 -0700

    its my world

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 /**
- * main-Prints the name of the programe followed by a new line
- * @argc: Size of the argumente array
- * @argv: The input argument array
- * Return: Zero as success
- */
+* main-Prints the name of the programe followed by a new line
+* @argc: Size of the argumente array
+* @argv: The input argument array
+* Return: Zero as success
+*/
 int main(int argc, char *argv[])
 {
-  (void)argc;
-
-  printf("%s\n", argv[0]);
-  return (0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
 
